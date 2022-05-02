6593915b355298f66ce59cce67da4ce894e61082
Filipe Gomes <filipe.gomes.3@globalhitss.com.br>
2022-05-02 20:22:33 -0300

    fazendo alteracoes

--- src/com/covert/isa/Main.java
+++ src/com/covert/isa/Main.java
@@ -7,13 +7,11 @@ import java.nio.charset.StandardCharsets;
 public class Main {
 
     static String nomeArq;
-    static int posicaoInicial = 0, posicaoFinal = 0, index = 0, indexD = 0;
-    static String nomeTabelaFilha = "";
     static String caminhoSaida = "";
     static String titulo = "Conversion Txt -> Excel";
 
     public static void main(String[] args) throws FileNotFoundException {
-        JOptionPane.showMessageDialog(null, "Seja bem vinda", titulo, 1);
+        JOptionPane.showMessageDialog(null, "Seja bem vindo(a)", titulo, -1);
         escolherArquivos(nomeArq);
         PrintStream saida = new PrintStream(new File(caminhoSaida));
         System.setOut(saida);
@@ -31,29 +29,31 @@ public class Main {
             }
             arq.close();
             JOptionPane.showMessageDialog(null, "Muito Obrigado, Concluído com Sucesso", titulo, 1);
+            JOptionPane.showMessageDialog(null, "Arquivo está em: "+caminhoSaida, titulo, 2);
         } catch (IOException e) {
             JOptionPane.showInputDialog("Erro na abertura do arquivo: %s.\n", e.getMessage());
         }
     }
-    public static String escolherArquivos(String entrada) {
+    public static void escolherArquivos(String entrada) {
         File[] arquivos = null;
         JFileChooser fc = new JFileChooser();
         fc.setDialogTitle("Escolha o arquivo para Conversão");
         fc.setDialogType(JFileChooser.OPEN_DIALOG);
-        fc.setApproveButtonText("Ok");
+        fc.setApproveButtonText("Ok");;
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
         fc.setMultiSelectionEnabled(true);
         int resultado = fc.showOpenDialog(fc);
+
         if (resultado == JFileChooser.CANCEL_OPTION) {
             System.exit(1);
         }
+
         arquivos = fc.getSelectedFiles();
         for (File f : arquivos) {
             nomeArq = (f.getParentFile() + "\\" + f.getName());
             String nameFile = f.getName().replace(".", "")+".xslx";
             caminhoSaida = (f.getParentFile() + "\\Modificado_" + nameFile );
         }
-        return null;
     }
 
 }
